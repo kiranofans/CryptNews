657fb8b2a5a40bea8f0c2a9a040ad595a16cb81b
Shu <sakurala111@yahoo.com>
2026-03-02 01:55:04 -0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/CryptNews/', // ← REPLACE with your GitHub repo name
+    base: '/', // ← REPLACE with your GitHub repo name
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
